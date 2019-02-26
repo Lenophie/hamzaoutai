2ffd244451a9668492d68371623d3f7f9ba9e328
Lenophie <quasiphies@gmail.com>
2019-02-27 00:03:37 +0100

    Add lighting controller

---
+++ Assets/Scripts/LightingController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class LightingController : MonoBehaviour
+{
+
+	[SerializeField] private float _speed;
+	
+	void Update()
+	{
+		transform.Rotate(new Vector3(_speed, _speed * 1.3f, 0f));
+	}
+}
