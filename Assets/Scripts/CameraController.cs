abafceebbf6750f5f350cd98b135dcb367fc1307
Lenophie <quasiphies@gmail.com>
2019-02-26 20:13:18 +0100

    Set camera vertical limits

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,6 +10,8 @@ public class CameraController : MonoBehaviour {
 	{
 		_yawn += Input.GetAxis("Mouse X") * _sensibility;
 		_pitch -= Input.GetAxis("Mouse Y") * _sensibility;
+		if (_pitch < -90f) _pitch = -90f;
+		else if (_pitch > 90f) _pitch = 90f;
 		transform.eulerAngles = new Vector3(_pitch, _yawn, 0f);
 	}
 }
