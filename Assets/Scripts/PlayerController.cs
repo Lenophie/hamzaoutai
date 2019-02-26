daeb3f3eeb399329f2d83f6f508d934b8b7cdf88
Lenophie <quasiphies@gmail.com>
2019-02-26 15:54:39 +0100

    Add movement logic to player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-
-	void Start()
-	{
-		
-	}
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private GameObject _player;
+	[SerializeField] private float _speed;
 	
 	void Update()
 	{
-		Debug.Log("hey");
+		Vector2 inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		_player.transform.position = new Vector3(_player.transform.position.x + inputVector.x * _speed,
+			_player.transform.position.y,
+			_player.transform.position.z + inputVector.y * _speed
+			);
 	}
 }
