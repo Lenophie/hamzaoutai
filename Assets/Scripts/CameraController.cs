41b2e5465046ab0a14da9ca3a7cf298e7b136211
Lenophie <quasiphies@gmail.com>
2019-02-26 20:07:32 +0100

    Add camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	[SerializeField] private float _sensibility;
+	private float _yawn = 0.0f;
+	private float _pitch = 0.0f;
+	
+	void Update()
+	{
+		_yawn += Input.GetAxis("Mouse X") * _sensibility;
+		_pitch -= Input.GetAxis("Mouse Y") * _sensibility;
+		transform.eulerAngles = new Vector3(_pitch, _yawn, 0f);
+	}
+}
